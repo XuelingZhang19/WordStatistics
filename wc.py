2711836d49a5473879a0094b517c4e44bceede84
Xueling Zhang <46972217+XuelingZhang19@users.noreply.github.com>
2021-03-21 18:41:58 -0500

    Feature 2 added
    Features:
    1. Unique word frequency
    2. Number of lines

--- wc.py
+++ wc.py
@@ -3,23 +3,31 @@ import sys
 
 def main(argv):
     file = argv[1]
-    textString = "\n".join(open(file).readlines())
-    print(textString)
-    print(type(textString))
-    wordlist = textString.split()
+    textString = open(file).read()
 
+
+    #===========feature 1: frequency of each unique word========
+    wordlist = textString.split()
     wordset = set()
     for word in wordlist:
         wordset.add(word)
-
     wordFreq = []
     for w in wordset:
         wordFreq.append(wordlist.count(w))
 
-    print("String\n" + textString + "\n")
-    print("Uniq words\n" + str(wordset) + "\n")
-    print("Frequencies\n" + str(wordFreq) + "\n")
-    print("Pairs\n" + str(list(zip(wordlist, wordFreq))))
+    print("===========String================\n" + textString + "\n")
+    # print("Uniq words\n" + str(wordset) + "\n")
+    print("==========feature 1: Uniqi word and frequency==========")
+    # print("Frequencies\n" + str(wordFreq) + "\n")
+    print(str(list(zip(wordset, wordFreq))) + "\n")
+
+
+    #===============featrure 2: LineCount =================
+    lineCount = textString.split("\n")
+    print("==========feature 2: Number of lines==========\n" + "Number of lines: " + str(len(lineCount)))
+
+
+
 
 if __name__ == "__main__":
     main(sys.argv)

73d12aa46a619aca5f496432aed8b183d8c00e87
Xueling Zhang <46972217+XuelingZhang19@users.noreply.github.com>
2021-03-24 13:17:05 -0500

    Add 1. specification 2. readme 3. test file

---
+++ readme.py
@@ -0,0 +1,2 @@
+To run the code, just run the python file "wc.py" under ./Word Statistics/,  with the text file that you want to analyze.
+command example: "python3 wc.py ./text"
--- wc.py
+++ wc.py
@@ -1,11 +1,14 @@
 import sys
 
-
 def main(argv):
     file = argv[1]
     textString = open(file).read()
+    wc(textString)
+    lc(textString)
+    cc(textString)
 
-    #===========feature 1: frequency of each unique word========
+#===========feature 1: Frequency of each unique word========
+def wc(textString):
     wordlist = textString.split()
     wordset = set()
     for word in wordlist:
@@ -20,13 +23,14 @@ def main(argv):
     # print("Frequencies\n" + str(wordFreq) + "\n")
     print(str(list(zip(wordset, wordFreq))) + "\n")
 
-    #===============featrure 2: LineCount =================
+# ===============featrure 2: LineCount =================
+def lc(textString):
     lineCount = textString.split("\n")
-    print("==========feature 2: Number of lines==========\n" + "Number of lines: " + str(len(lineCount)))
+    print("==========feature 2: Number of lines==========\n" + "Number of lines: " + str(len(lineCount)) + "\n")
 
-
-    #===============featrure 3: CharCount =================
-    print("==========feature 3: Number of Characters==========\n" + "Number of Characters: " + str(len(textString)))
+#===============featrure 3: CharCount =================
+def cc(textString):
+    print("==========feature 3: Number of Characters==========\n" + "Number of Characters: " + str(len(textString)) + "\n")
 
 
 if __name__ == "__main__":
